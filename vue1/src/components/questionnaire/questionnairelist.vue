36d5691129122a02753adcf39df59eea5cb6d228
buaa19231051 <70186407+buaa19231051@users.noreply.github.com>
2021-08-22 23:00:11 +0800

    函数参数改变

--- vue1/src/components/questionnaire/questionnairelist.vue
+++ vue1/src/components/questionnaire/questionnairelist.vue
@@ -26,9 +26,9 @@
                                 <div>
                                     <el-button type="text" class="el-icon-view" @click="checkClick(props.row.id)">预览</el-button>
                                     <el-button type="text" class="el-icon-edit" @click="designClick(props.row)">设计问卷</el-button>
-                                    <el-button type="text" class="el-icon-data-analysis" @click="deleteClick(props.row.id)">查看数据分析</el-button>
-                                    <el-button type="text" class="el-icon-download" @click="checkClick(props.row.id)">导出</el-button>
-                                    <el-button type="text" size="small" class="el-icon-document-copy" @click="copyquestionnaire(props.row.id)">复制</el-button>
+                                    <el-button type="text" class="el-icon-data-analysis" @click="deleteClick(props.row)">查看数据分析</el-button>
+                                    <el-button type="text" class="el-icon-download" @click="checkClick(props.row)">导出</el-button>
+                                    <el-button type="text" size="small" class="el-icon-document-copy" @click="copyquestionnaire(props.row)">复制</el-button>
                                     <el-button type="text" size="small" class="el-icon-delete" @click="deletequestionnaire(props.row)">删除</el-button>
                                 </div>
                             </el-form-item>
@@ -95,7 +95,7 @@
 		  this.loadquestionnaire()
 		},
         methods:{
-            checkreport(){
+            checkreport(row){
                 var _this=this
                 this.$router.push({path: "/report"});
             },
